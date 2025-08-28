63cb1de744747fd172897af1c2067ac00ec054ab
Atnatewoss <icodeyt21@gmail.com>
2025-08-28 09:58:03 +0300

    Update hopfield-network-mnist.py

--- backend/hopfield-network-mnist.py
+++ backend/hopfield-network-mnist.py
@@ -32,6 +32,7 @@ def find_weight(one_d_array):
     N = one_d_array[0].shape[0]       # number of neurons/pixels
     W = torch.zeros((N, N))           # NxN zero matrix
 
+    # connectivity/symmetric weights with valid enery landscape computed, where the stored patterns are attractors
     for pattern in one_d_array:
         W += torch.ger(pattern, pattern)  # outer product
 
@@ -45,6 +46,7 @@ print("Weight matrix shape:", W.shape)
 # --- Step-3: Hopfield recall function ---
 def recall(W, input_pattern, steps=5):
     x = input_pattern.clone()
+    # Iteration and recall function
     for _ in range(steps):
         # synchronous update rule: x <- sign(Wx)
         x = torch.sign(W @ x)
@@ -53,10 +55,12 @@ def recall(W, input_pattern, steps=5):
 
 # --- Step-4: Test with one image ---
 original, _ = mnist[5]   # pick an image
+# this line flattens the image b/c we're about to insert this 2D image into our hopfield network and hopfield only works with 1D neurons, not 2D matrices
 original = original.view(-1)
 original = torch.where(original > 0.5, 1, -1).float()
 
 # distort it (flip some pixels randomly)
+# Noisy one puts the system in a higher-enery state near a valley.
 noisy = original.clone()
 noise = torch.randint(0, 100, noisy.shape) < 10  # 10% noise
 noisy[noise] *= -1
@@ -81,6 +85,6 @@ import os
 os.makedirs("frontend/public", exist_ok=True)  # create folder if it doesn't exist
 
 # convert from bipolar {-1,+1} to 0-1 for grayscale display
-plt.imsave("frontend/public/original.png", (original.view(28,28).numpy() + 1)/2, cmap="gray")
-plt.imsave("frontend/public/noisy.png", (noisy.view(28,28).numpy() + 1)/2, cmap="gray")
-plt.imsave("frontend/public/recalled.png", (recalled.view(28,28).numpy() + 1)/2, cmap="gray")
+plt.imsave("../frontend/public/original.png", (original.view(28,28).numpy() + 1)/2, cmap="gray")
+plt.imsave("../frontend/public/noisy.png", (noisy.view(28,28).numpy() + 1)/2, cmap="gray")
+plt.imsave("../frontend/public/recalled.png", (recalled.view(28,28).numpy() + 1)/2, cmap="gray")
